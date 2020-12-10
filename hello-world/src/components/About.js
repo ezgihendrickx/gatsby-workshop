c3b08dde710e1cf17ec849abab1b9c45cd84d490
ezgihendrickx <ezgihendrickx@gmail.com>
2020-12-10 22:43:23 +0300

    Add badges

--- hello-world/src/components/About.js
+++ hello-world/src/components/About.js
@@ -6,6 +6,38 @@ function About() {
     <div className="about">
       <img className="reindeer" src="./img/reindeer.svg" alt="reindeer"></img>
       <h2>ABOUT </h2>
+      <div>
+        <img
+          className="profile"
+          src="./img/meblack&white.jpg"
+          alt="profilephoto"
+        ></img>
+      </div>
+
+      <div className="aboutme">
+        <h3 className="header3">Hello there, I'm Ezgi!</h3>
+        <div className="bio">
+          <p>
+            {" "}
+            I am currently looking for my next challenge as a Front-End
+            Developer, having expertise in both Web Development and UI/UX
+            design. After moving to Belgium I decided to return to my passion
+            about design & advertising and further build upon my university
+            studies in Communication Arts.
+          </p>
+
+          <p>
+            After an internship as UI/UX designer at an ambitious start-up
+            company within EY, I am currently at the end of an intensive 7 month
+            Education in Web Development @BeCode.
+          </p>
+          <p>
+            During my stay in Stockholm, Sweden, I got fascinated by Nordic
+            design and since then I integrate minimalist design principles in
+            many of my projects.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
--- hello-world/src/components/Skills.js
+++ hello-world/src/components/Skills.js
@@ -4,17 +4,66 @@ import "./skills.styles.scss"
 function Skills() {
   return (
     <div className="skills">
-      <img
-        className="hat"
-        src="./img/graduation-hat.svg"
-        alt="graduation-hat"
-      ></img>
-      <h2>SKILLS </h2>
-      <div className="badges">
-        <img className="html" src="./img/html5.svg" alt="HTML5"></img>
+      <div className="skillsheader">
+        <img
+          className="hat"
+          src="./img/graduation-hat.svg"
+          alt="graduation-hat"
+        ></img>
+        <h2>SKILLS </h2>
       </div>
-      <div className="htmlbadge">
-        <p>HTML</p>
+      <div className="badges">
+        <div className="firstdiv">
+          <div className="char">
+            <img className="html" src="./img/html5.svg" alt="HTML5 badge"></img>
+            <p>HTML</p>
+          </div>
+          <div className="char">
+            <img className="css" src="./img/css-3.svg" alt="CSS badge"></img>
+            <p>CSS</p>
+          </div>
+          <div className="char">
+            <img className="js" src="./img/js.jpg" alt="JS badge"></img>
+
+            <p>JS</p>
+          </div>
+          <div className="char">
+            <img
+              className="react"
+              src="./img/react.png"
+              alt="react badge"
+            ></img>
+
+            <p>React</p>
+          </div>
+        </div>
+        <div className="seconddiv">
+          <div className="char">
+            <img className="git" src="./img/git.png" alt="git badge"></img>
+            <p>Git</p>
+          </div>
+          <div className="char">
+            <img className="vsc" src="./img/vsc.png" alt="vscode badge"></img>
+
+            <p>VSC</p>
+          </div>
+          <div className="char">
+            <img
+              className="sketch"
+              src="./img/sketch.png"
+              alt="sketch badge"
+            ></img>
+            <p>Sketch</p>
+          </div>
+          <div className="char">
+            <img
+              className="figma"
+              src="./img/figma.png"
+              alt="figma badge"
+            ></img>
+            <p>Figma</p>
+          </div>
+        </div>
       </div>
     </div>
   )
