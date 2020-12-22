9d7e94f31917c4ae2094ae2654250ed5d74fb68e
ezgihendrickx <ezgihendrickx@gmail.com>
2020-12-22 14:31:13 +0300

    Add publicfolder

--- hello-world/src/components/Cards.js
+++ hello-world/src/components/Cards.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "./cards.styles.scss"
 
-function Cards(props) {
+function Cards() {
   return (
     <div className="cards">
       <h3>Todo List</h3>
@@ -28,7 +28,7 @@ function Cards(props) {
             />
           </a>
         </p>
-        <p>This is my very first minimalist Todo List. Check it out</p>
+        <p>This is my first Todo List. Check it out!</p>
         <button>
           <strong>Live</strong>
         </button>
